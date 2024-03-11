367384ce35c3ef1ced3eba6bc6df957410ab1800
Peter Gregus <peter.gregus@sap.com>
2024-03-11 16:46:19 +0100

    Refactor main method

--- src/TheatricalPlayersRefactoringKata/IPrinter.cs
+++ src/TheatricalPlayersRefactoringKata/IPrinter.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TheatricalPlayersRefactoringKata
 {
-    internal interface IPrinter
+    public interface IPrinter
     {
         public string Print(Invoice invoice, Dictionary<string, Play> plays);
     }
