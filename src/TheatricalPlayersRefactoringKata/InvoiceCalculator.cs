cbb9442365a0f34b61012e8821fdaac61c205906
Peter Gregus <peter.gregus@sap.com>
2024-03-11 17:00:23 +0100

    added invoice calculator

---
+++ src/TheatricalPlayersRefactoringKata/InvoiceCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheatricalPlayersRefactoringKata
+{
+    public static class InvoiceCalculator
+    {
+        public static int CalculateAmount(Performance perf, Play play)
+        {
+            var thisAmount = 0;
+            switch (play.Type)
+            {
+                case "tragedy":
+                    thisAmount = 40000;
+                    if (perf.Audience > 30)
+                    {
+                        thisAmount += 1000 * (perf.Audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    thisAmount = 30000;
+                    if (perf.Audience > 20)
+                    {
+                        thisAmount += 10000 + 500 * (perf.Audience - 20);
+                    }
+                    thisAmount += 300 * perf.Audience;
+                    break;
+                default:
+                    throw new Exception("unknown type: " + play.Type);
+            }
+            return thisAmount;
+        }
+    }
+}
